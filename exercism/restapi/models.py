6f70cbe63a68f02fb8f8e21f3d456a1af0d08cae
Abhishek-N <abhisheknshetty97@gmail.com>
2022-12-24 10:58:33 +0530

    added models

--- exercism/restapi/models.py
+++ exercism/restapi/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    name = models.CharField(max_length=100)
+    owes = models.JSONField(blank=True)
+    owed_by = models.JSONField(blank=True)
+    balance = models.FloatField(blank=True)
+
+    class Meta:
+        db_table = "users"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
--- exercism/restapi/views.py
+++ exercism/restapi/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Users
 
 # Create your views here.
+
+
+class UsersViews(APIView):
+
+    def get(self, request):
+        users = [user.name for user in Users.objects.all()]
+        return Response(users)
